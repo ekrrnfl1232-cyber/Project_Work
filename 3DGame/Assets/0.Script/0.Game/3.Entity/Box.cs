9fee8a9b3b5c1e805d55484a52d58a63fa35d314
ekrrnfl1232-cyber <ekrrnfl1232@gmail.com>
2026-09-04 15:51:39 +0900

    3DGame 09/04

--- 3DGame/Assets/0.Script/0.Game/3.Entity/Box.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Box : MonoBehaviour, IInterectable
-{
-    public void Interact()
-    {
-        Debug.Log("상자 상호작용");
-    }
-}
--- 3DGame/Assets/0.Script/0.Game/3.Entity/NPC/NPC.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class NPC : MonoBehaviour, IInterectable
-{
-    public void Interact()
-    {
-        Debug.Log("¥Î»≠");
-    }
-}
